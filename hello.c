3e78fbe87506c56ab410faa47078dd38c2688360
Jules <32022606+Jasbury2@users.noreply.github.com>
2018-08-23 14:51:15 -0500

    Add files via upload
    Adding hello file

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Julie Asbury
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Julie, Computer Science Major, \n");
+
+  return 0;
+}
