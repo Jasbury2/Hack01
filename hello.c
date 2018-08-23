d8d5fcfd88be340668f6be9e13fdd95a230bf251
Carlie Belt <carlie.r.belt@gmail.com>
2018-08-23 14:55:28 -0500

    added name and major

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Julie, Computer Science Major, Haley Shaw, Computer Engineering Major\n");
+  printf("Julie, Computer Science Major, Haley Shaw, Computer Engineering Major, Carlie, Computer Engineering\n");
 
   return 0;
 }
